759f1956e8d35cb181ffa9530f3591363bc0c73e
michellecgude <michellecgude@gmail.com>
2020-12-18 02:33:50 -0600

    finished dummy login via prompts, function that returns two innerHTML values, while loop with alert that continues to prompt user for input.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,7 @@
 // COVID DATA API:
 const covidURL = "https://api.covid19api.com/summary";
 
-// set potential variables needed:    
+// API Variables:    
 let covidData = document.querySelectorAll(".num-data");
 const confirmData = document.querySelector(".confirm-num");
 const deathData = document.querySelector(".death-num");
@@ -32,4 +32,30 @@ fetch(covidURL, {headers: {"x-access-token": "5cf9dfd5-3449-485e-b5ae-70a60e9978
     .then(response => {totalrecovData.innerHTML = response.Global.TotalRecovered;})
     .catch(err => console.log(`Sorry, the total recovered COVID data isn't available: ${err}`))
 
-// functions
+
+// JS Interactivity:
+
+// Created Dummy Login via prompt, function that changes HTML, as well as while loop that continues to prompt the user for an input). 
+// NOTE: This is for MVP functionality. If I have the time, I really want to use either a modal, or even create a greeting page (ie, separate HTML file that remembers the name input and transfers it over to the general interface of Wallflower. But for now, this will do for the MVP.)
+let welcomePrompt = prompt("Welcome to Wallflower. Happy you could join us. Let's get you set up. But first, what name do you go by?");
+
+function mvpGreeting() {
+    let welcomeGreeting = document.querySelector(".headline-text").innerHTML = (`Welcome to Wallflower, ${welcomePrompt}.`);
+    let adviceGreeting = document.querySelector(".advice-header").innerHTML = (`Here's your advice of the day, ${welcomePrompt}:`)
+
+    return {
+        welcomeGreeting,
+        adviceGreeting
+    };
+}
+
+while (welcomePrompt == null || undefined) {
+    welcomePrompt = prompt("Please enter a name to experience Wallflower to it's best potential.");
+
+    if (welcomePrompt !== null || undefined) {
+        alert(`Welcome in! Let's start recovering from the COVID blues together, ${welcomePrompt}.`)
+        mvpGreeting();
+    }
+}
+    
+
