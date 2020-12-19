38086627bdb556e354e19fa9002f5ff16baf70f2
michellecgude <michellecgude@gmail.com>
2020-12-18 18:43:04 -0600

    COVID-19 API Data is formatted correctly.

--- resources/js/script.js
+++ resources/js/script.js
@@ -8,29 +8,63 @@ const deathData = document.querySelector(".death-num");
 const recoverData = document.querySelector(".recover-num");
 const totalrecovData = document.querySelector(".totalrecov-num");
 
+// Fetch COVID Data:
 // confirm data api set to html:
 fetch(covidURL, {headers: {"x-access-token": "5cf9dfd5-3449-485e-b5ae-70a60e997864"}})
     .then(response => response.json())
-    .then(response => {confirmData.innerHTML = response.Global.NewConfirmed;})
+    .then(response => {
+        let formatConfirmed = response.Global.NewConfirmed;
+        confirmData.textContent = formatConfirmed.toLocaleString("en-US");
+    })
     .catch(err => console.log(`Sorry, the confirmed COVID cases data isn't available: ${err}`))
+
+
 
 // death data api set to html:
 fetch(covidURL, {headers: {"x-access-token": "5cf9dfd5-3449-485e-b5ae-70a60e997864"}})
     .then(response => response.json())
-    .then(response => {deathData.innerHTML = response.Global.NewDeaths;})
+    .then(response => {
+        let formatDeath = response.Global.NewDeaths;
+        deathData.textContent = formatDeath.toLocaleString("en-US");
+    })
     .catch(err => console.log(`Sorry, the new deaths COVID data isn't available: ${err}`))
 
 // recover data api set to html:
 fetch(covidURL, {headers: {"x-access-token": "5cf9dfd5-3449-485e-b5ae-70a60e997864"}})
     .then(response => response.json())
-    .then(response => {recoverData.innerHTML = response.Global.NewRecovered;})
+    .then(response => {
+        let formatRecovered = response.Global.NewRecovered;
+        recoverData.textContent = formatRecovered.toLocaleString("en-US");
+    })
     .catch(err => console.log(`Sorry, the new recovered COVID data isn't available: ${err}`))
 
 // total recovered data api set to html:
 fetch(covidURL, {headers: {"x-access-token": "5cf9dfd5-3449-485e-b5ae-70a60e997864"}})
     .then(response => response.json())
-    .then(response => {totalrecovData.innerHTML = response.Global.TotalRecovered;})
+    .then(response => {
+        let formatTotalRecov = response.Global.TotalRecovered;
+        totalrecovData.textContent = formatTotalRecov.toLocaleString("en-US");
+    })
     .catch(err => console.log(`Sorry, the total recovered COVID data isn't available: ${err}`))
+
+
+// DATA TO FORMATTED NUMERALS: 
+/* 
+
+
+Use :  miscellaneous.toLocalString("en-IN");
+
+document.write(miscellaneous.toLocaleString("en-IN"));
+*/ 
+
+
+
+
+
+
+
+
+
 
 
 
@@ -38,9 +72,14 @@ fetch(covidURL, {headers: {"x-access-token": "5cf9dfd5-3449-485e-b5ae-70a60e9978
 const adviceURL = "https://api.adviceslip.com/advice"
 
 // Advice API Variables:
+const adviceText = document.querySelector(".advice-text");
+const adviceButton = document.querySelector(".btn-advice");
+
+// Fetch API Data:
+// generic quote fetch
 fetch(adviceURL)
     .then(response => response.json())
-    .then(response => console.log(`You're lucky today! Here's some advice just for you ${response.slip.advice}`))
+    .then(response => {adviceText.innerHTML = response.slip.advice})
     .catch(err => console.log(`I'm sorry, there's no advice for today ${err}`))
 
 
@@ -49,6 +88,21 @@ fetch(adviceURL)
 
 
 // JS Interactivity:
+
+// Button to toggle/randomized advice:
+adviceButton.onclick = function (event) {
+    event.preventDefault();
+
+    fetch(adviceURL)
+    .then(response => response.json())
+    .then(response => {adviceText.innerHTML = response.slip.advice})
+    .catch(err => console.log(`I'm sorry, there's no advice for today ${err}`))
+
+}
+
+
+
+// Dummy login via prompts that affect HTML:
 // let welcomePrompt = prompt("Welcome to Wallflower. Happy you could join us. Let's get you set up. But first, what name do you go by?");
 
 // function mvpGreeting() {
@@ -65,7 +119,6 @@ fetch(adviceURL)
 //         alert(`Welcome in! Let's start recovering from the COVID blues together, ${welcomePrompt}.`)
 //         mvpGreeting();
 //     }
-
 
 
 // OLD BUGGY CODE THAT ATTEMPTED TO FIX INITIAL BUGS: 
