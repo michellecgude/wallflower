b2c9739fc5da36c6f3e338912ba47782f53a3d7e
michellecgude <michellecgude@gmail.com>
2020-12-20 16:56:59 -0600

    sunday work so far: deleted original dummy login and replaced it with welcome page created new html file and new js file downloaded jquery library created welcome page with fadein/fadeout (jquery and JS) created dummy login/name/cookie with query string

---
+++ resources/js/userinterfacescript.js
@@ -0,0 +1,81 @@
+// - - - USER INTERFACE SCRIPT - - - //
+
+// // COVID DATA API URL:
+const covidURL = "https://api.covid19api.com/summary";
+
+// // COVID API Variables:    
+let covidData = document.querySelectorAll(".num-data");
+const confirmData = document.querySelector(".confirm-num");
+const deathData = document.querySelector(".death-num");
+const recoverData = document.querySelector(".recover-num");
+const totalrecovData = document.querySelector(".totalrecov-num");
+
+// // Fetch COVID Data:
+// // confirm data api set to html:
+fetch(covidURL, {headers: {"x-access-token": "5cf9dfd5-3449-485e-b5ae-70a60e997864"}})
+.then(response => response.json())
+.then(response => {
+    let formatConfirmed = response.Global.NewConfirmed;
+    confirmData.textContent = formatConfirmed.toLocaleString("en-US");
+})
+.catch(err => console.log(`Sorry, the confirmed COVID cases data isn't available: ${err}`))
+
+// // death data api set to html:
+fetch(covidURL, {headers: {"x-access-token": "5cf9dfd5-3449-485e-b5ae-70a60e997864"}})
+.then(response => response.json())
+.then(response => {
+    let formatDeath = response.Global.NewDeaths;
+    deathData.textContent = formatDeath.toLocaleString("en-US");
+})
+.catch(err => console.log(`Sorry, the new deaths COVID data isn't available: ${err}`))
+
+// // recover data api set to html:
+fetch(covidURL, {headers: {"x-access-token": "5cf9dfd5-3449-485e-b5ae-70a60e997864"}})
+.then(response => response.json())
+.then(response => {
+    let formatRecovered = response.Global.NewRecovered;
+    recoverData.textContent = formatRecovered.toLocaleString("en-US");
+})
+    .catch(err => console.log(`Sorry, the new recovered COVID data isn't available: ${err}`))
+    
+    //     // total recovered data api set to html:
+    fetch(covidURL, {headers: {"x-access-token": "5cf9dfd5-3449-485e-b5ae-70a60e997864"}})
+.then(response => response.json())
+.then(response => {
+    let formatTotalRecov = response.Global.TotalRecovered;
+    totalrecovData.textContent = formatTotalRecov.toLocaleString("en-US");
+})
+.catch(err => console.log(`Sorry, the total recovered COVID data isn't available: ${err}`))
+
+
+// // RANDOM ADVICE API URL:
+const adviceURL = "https://api.adviceslip.com/advice"
+
+// // Advice API Variables:
+const adviceText = document.querySelector(".advice-text");
+const adviceButton = document.querySelector(".btn-advice");
+
+// // Fetch API Data:
+// // generic quote fetch
+fetch(adviceURL)
+.then(response => response.json())
+.then(response => {adviceText.innerHTML = response.slip.advice})
+.catch(err => console.log(`I'm sorry, there's no advice for today ${err}`))
+
+
+
+// JS Interactivity:
+
+// Button to toggle/randomized advice:
+adviceButton.onclick = function (event) {
+    event.preventDefault();
+    
+    fetch(adviceURL)
+    .then(response => response.json())
+    .then(response => {adviceText.innerHTML = response.slip.advice})
+    .catch(err => console.log(`I'm sorry, there's no advice for today ${err}`))
+}
+
+
+// FUTURE JQUERY SCRIPT WILL GO HERE.
+document.getElementById("headline-text").innerHTML = ("How're you feeling today?"); // parses string to just showcase data.
---
+++ resources/js/welcomescript.js
@@ -0,0 +1,22 @@
+// - - - WELCOME TO WALLFLOWER SCRIPT - - - //
+
+// JQUERY DUMMY USER LOGIN: 
+// using query string to grab name greeting...: (will use cookies in the future once I learn how to use them. Just using this as a loophole because I have limited time with the project.)
+
+$("#greet-button").click(function(event) {
+    event.preventDefault();
+
+    $("#login-prompt").fadeOut("slow");
+    $("#login-form").fadeOut("slow");
+
+    $(".wallflower-greeting").delay(200).fadeIn(4000).fadeOut(4000);
+    $(".welcome-to-wallflower").delay(900).fadeIn(4000).fadeOut(3000);
+
+    // welcome to wallflower fade-in greeting:
+    document.getElementById("greeting").innerHTML = (`Nice to meet you, ${window.location.search.substring(12)}.`); // parses string to just showcase data.
+    document.getElementById("welcome-in").innerHTML = ("Welcome to Wallflower."); // parses string to just showcase data.
+
+
+    window.setTimeout(function () {
+        window.location.href = "userinterface.html"; }, 8000); 
+});
